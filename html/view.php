e55e94a01b0b35a88a7ecf8102e2d2846a6d8209
mmoreno43 <marcusmoreno2343@gmail.com>
2020-02-08 15:30:28 -0800

    added table and styles

--- html/view.php
+++ html/view.php
@@ -1,10 +1,19 @@
-<!DOCTYPE html>
 <html>
 <body>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
 <a href='index.php'>Home</a><br>
 
-<h2>View User</h2>
+<h2>View Guests</h2>
 
+<table class="striped">
+            <tr class="header">
+                <td>ID</td>
+                <td>Firstname</td>
+                <td>Lastname</td>
+                <td>Phone Number</td>
+            </tr>
 <?php
 include_once('mysql.php');
 
@@ -13,11 +22,16 @@ $result = mysqli_query($conn, $ins);
 
 if (mysqli_num_rows($result) > 0) {
     while($row = mysqli_fetch_assoc($result)) {
-        echo "id: " . $row['id']. " - Name: " . $row['firstname']. " " . $row['lastname']. " Number: ". $row['phone']." <br>";
-    }
+        echo "<tr>";
+                   echo "<td>".$row["id"]."</td>";
+                   echo "<td>".$row["firstname"]."</td>";
+                   echo "<td>".$row["lastname"]."</td>";
+                   echo "<td>".$row["phone"]."</td>";
+                   echo "</tr>";
+               }
 }
 
 ?>
-
+</table>
 </body>
 </html>
